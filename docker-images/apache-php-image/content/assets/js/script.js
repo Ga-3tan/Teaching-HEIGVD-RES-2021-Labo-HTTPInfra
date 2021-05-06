0239515b3788c14d95c99c7f6321071cc52961ff
Gaétan Zwick <gaetan.zwick@heig-vd.ch>
2021-05-06 11:59:14 +0200

    finished step 1, no report

---
+++ docker-images/apache-php-image/content/assets/js/script.js
@@ -0,0 +1,17 @@
+var element = document.getElementById("infoSection");
+element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("topBtn").style.display = "block";
+  } else {
+    document.getElementById("topBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
